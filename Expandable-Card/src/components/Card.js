96b340215c777cb8395b43bdc69ba261e1758119
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-03-15 16:53:03 +0530

    Add files via upload
    Project Finished

--- Expandable-Card/src/components/Card.js
+++ Expandable-Card/src/components/Card.js
@@ -1,21 +1,35 @@
 import {motion} from "framer-motion";
-import { useState } from "react";
+import {useState} from "react";
 
 const Card = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <motion.div className="card" variants={cardVariant}>
-      <h2>Framer Motion 🚀</h2>
-      <motion.div variants={textVariant} initial="shrink" animate="expand">
-        <motion.p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </motion.p>
-        <motion.p>
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
-        </motion.p>
-      </motion.div>
+    <motion.div
+      layout
+      transition={{layout: {duration: 0.5, type: "spring"}}}
+      className="card"
+      style={{borderRadius: "1rem", boxShadow: "0px 10px 30px rgba(0 0 0 0.5)"}}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+
+      {isOpen && (
+        <motion.div
+          layout="position"
+          initial={{opacity: 0}}
+          animate={{opacity: 1, transition: {duration: 0.5}}}
+        >
+          <motion.p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </motion.p>
+          <motion.p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </motion.p>
+        </motion.div>
+      )}
     </motion.div>
   );
 };
