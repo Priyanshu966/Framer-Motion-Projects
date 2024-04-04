21a47b32c094d8e1f832dec49d401b4b0abc1408
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-04-04 14:40:30 +0530

    Add files via upload
    Animated slider

---
+++ Infinite-Carousel/src/App.js
@@ -0,0 +1,11 @@
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-950 grid place-items-center ">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ Infinite-Carousel/src/components/Carousel.js
@@ -0,0 +1,98 @@
+import {useState} from "react";
+import {motion, AnimatePresence} from "framer-motion";
+import {RxTriangleRight} from "react-icons/rx";
+import {RxTriangleLeft} from "react-icons/rx";
+import {images} from "../assets/image";
+
+const Carousel = () => {
+  const [{index, direction}, setIsImage] = useState({index: 0, direction: 0});
+
+  const handleNext = (dir) => {
+    let newIndex = index + dir;
+    if (newIndex >= images.length) {
+      newIndex = 0;
+    }
+    setIsImage({index: newIndex, direction: dir});
+  };
+  const handlePrev = (dir) => {
+    let newIndex = index + dir;
+    if (newIndex < 0) {
+      newIndex = images.length - 1;
+    }
+    setIsImage({index: newIndex, direction: dir});
+  };
+
+  // const variants = {
+  //   initial: (direction) => {
+  //     return {x: direction > 0 ? 1000 : -1000, opacity: 0};
+  //   },
+  //   animate: () => {
+  //     return {x: 0, opacity: 1, zIndex: 1};
+  //   },
+  //   exit: (direction) => {
+  //     return {x: direction > 0 ? -1000 : 1000, opacity: 0, zIndex: 0};
+  //   },
+  //   transition: {
+  //     type: "spring",
+  //     damping: 30,
+  //     stiffness: 300,
+  //     opacity: {duration: 0.2},
+  //     x: {duration: 0.3},
+  //   },
+  // };
+
+  const variants = {
+    initial: (direction) => {
+      return {
+        x: direction > 0 ? 1000 : -1000,
+        opacity: 0,
+      };
+    },
+    animate: {
+      zIndex: 1,
+      x: 0,
+      opacity: 1,
+    },
+    exit: (direction) => {
+      return {
+        zIndex: 0,
+        x: direction < 0 ? 1000 : -1000,
+        opacity: 0,
+      };
+    },
+  };
+  return (
+    <div className="w-[500px] h-[300px] relative overflow-hidden">
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+          key={index}
+          src={images[index]}
+          custom={direction}
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            x: {type: "spring", stiffness: 300, damping: 30},
+            opacity: {duration: 0.2},
+          }}
+          className="w-full h-[100%] absolute left-0 top-0"
+        />
+      </AnimatePresence>
+
+      <button
+        onClick={() => handleNext(1)}
+        className="rounded-full bg-white w-10 h-10 grid place-items-center absolute top-1/2 right-2 -translate-y-1/2 z-20"
+      >
+        <RxTriangleRight />
+      </button>
+      <button
+        onClick={() => handlePrev(-1)}
+        className="rounded-full bg-white w-10 h-10 grid place-items-center absolute top-1/2 left-2 -translate-y-1/2 z-20"
+      >
+        <RxTriangleLeft />
+      </button>
+    </div>
+  );
+};
+export default Carousel;
