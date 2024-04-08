112a14f3a652c30c7831f291228fa4b5d1789879
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-04-08 17:08:39 +0530

    Add files via upload
    Aurora effect completed

--- Aurora-Effect/src/App.js
+++ Aurora-Effect/src/App.js
@@ -4,6 +4,8 @@ import {
   useMotionTemplate,
   animate,
 } from "framer-motion";
+import {Canvas} from "@react-three/fiber";
+import {Stars} from "@react-three/drei";
 import {IoArrowForwardOutline} from "react-icons/io5";
 import {useEffect} from "react";
 
@@ -27,13 +29,15 @@ function App() {
   return (
     <motion.div
       style={{backgroundImage}}
-      className="w-screen min-h-screen bg-gray-950 grid place-content-center text-white relative px-6"
+      className="w-screen min-h-screen bg-gray-950 grid place-content-center text-white relative px-6 font-sans"
     >
-      <div className="grid place-items-center max-w-[550px] gap-y-2 relative bottom-6">
+      <div className="grid place-items-center max-w-[550px] gap-y-2 relative bottom-6 z-20">
         <button className="capitalize bg-gray-800 w-fit mx-auto px-3 py-1.5 rounded-full text-[10px]">
           beta now live !
         </button>
-        <h1 className="text-6xl text-center">Build whatever u want</h1>
+        <h1 className="text-6xl text-center font-medium">
+          Build whatever u want
+        </h1>
         <p className="text-center my-4 text-sm">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -43,10 +47,24 @@ function App() {
           whileTap={{scale: 0.9}}
           transition={{type: "spring"}}
           style={{border, boxShadow}}
-          className="flex justify-center items-center w-fit mx-auto rounded-full px-4 py-1 text-[12px] font-semibold"
+          className="flex justify-center items-center w-fit mx-auto rounded-full px-4 py-1 text-[12px] font-semibold group"
         >
-          Start free trial <IoArrowForwardOutline className="ml-1" />
+          Start free trial{" "}
+          <IoArrowForwardOutline className="ml-1 group-hover:-rotate-45 group-active:rotate-0 transition-all ease-linear duration-300" />
         </motion.button>
+      </div>
+      <div className='absolute inset-0 z-0'>
+        <Canvas>
+          <Stars
+            radius={100}
+            depth={50}
+            count={5000}
+            factor={4}
+            saturation={0}
+            fade
+            speed={1}
+          />
+        </Canvas>
       </div>
     </motion.div>
   );
